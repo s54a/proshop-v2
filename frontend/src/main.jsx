6a9ab9490d554752d4ca8fa3ab08d4002e13554e
Sooraj Gupta <soorajgupta00@gmail.com>
2024-02-12 10:39:02 +0530

    created routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,22 @@
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
