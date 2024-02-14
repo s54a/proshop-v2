c5bf8abcb4be7c2bcecdeb592046e365c220218c
Sooraj Gupta <soorajgupta00@gmail.com>
2024-02-14 07:00:49 +0530

    created routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -22,14 +22,7 @@ app.get("/", (req, res) => {
   res.send("Working");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
