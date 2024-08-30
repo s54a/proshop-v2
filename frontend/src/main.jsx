de41f296cf246ff31e05e8088cecd7f6cacd461d
Sooraj Gupta <soorajgupta00@gmail.com>
2024-08-31 03:52:01 +0530

    added productScreen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,13 @@ import {
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />}></Route>
+      <Route path="/product/:id" element={<ProductScreen />}></Route>
     </Route>
   )
 );
