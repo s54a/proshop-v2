8f3ce170e6d14c39cf392c42b1a5d3e6071cb7c1
Sooraj Gupta <soorajgupta00@gmail.com>
2024-09-02 04:27:33 +0530

    dotenv setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/product.js";
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
