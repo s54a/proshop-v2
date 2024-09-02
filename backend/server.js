58e146ede04cda4c74de21ed10709abf297ea5a3
Sooraj Gupta <soorajgupta00@gmail.com>
2024-09-03 04:28:26 +0530

    created models

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import products from "./data/product.js";
-
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
