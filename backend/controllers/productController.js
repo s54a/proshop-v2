625f49a00464a3e8d17c9fd901aa5fe8cabad976
Sooraj Gupta <soorajgupta00@gmail.com>
2024-09-09 02:53:31 +0530

    created controllers cleaned up the productRouteFile

---
+++ backend/controllers/productController.js
@@ -0,0 +1,31 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+/**
+ * @desc Fetch All Products
+ * @route GET /api/products
+ * @access Public
+ */
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+/**
+ * @desc Fetch a Products by ID
+ * @route GET /api/products/:id
+ * @access Public
+ */
+const getProductsById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource Not Found");
+  }
+});
+
+export { getProducts, getProductsById };
