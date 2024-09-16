25aae70db2bf3a862d1a7b86635bd84901c19d39
Sooraj Gupta <soorajgupta00@gmail.com>
2024-09-16 18:28:29 +0530

    typo

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -33,14 +33,14 @@ export const cartSlice = createSlice({
 
       // Calculate Item Price
       state.itemPrice = addDecimals(
-        state.itemPrice.reducer((acc, item) => acc + item.price * item.qty, 0)
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
       );
 
       // Calculate Shipping Price (If order is over $100 then free shipping or else $10)
       state.shippingPrice = addDecimals(state.itemPrice > 100 ? 0 : 10);
 
       // Calculate Tax Price
-      state.taxPrice = addDecimals(Number(0.15 * state.itemPrice.toFixed(2)));
+      state.taxPrice = addDecimals(Number((0.15 * state.itemPrice).toFixed(2)));
 
       // Calculate Total Price
       state.totalPrice = (
