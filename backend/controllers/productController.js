c25c616f76e156dc105166409a69ae00d72ed524
Sooraj Gupta <soorajgupta00@gmail.com>
2024-09-20 04:40:05 +0530

    typo

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,6 @@ import Product from "../models/productModel.js";
  * @route GET /api/products
  * @access Public
  */
-
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
