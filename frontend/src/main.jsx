7f1dcbd8a61b0fa1166e61deec12c8dad2de7463
Sooraj Gupta <soorajgupta00@gmail.com>
2024-09-30 00:09:05 +0530

    created login screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import "./assets/styles/index.css";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
 import CartScreen from "./screens/CartScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />}></Route>
       <Route path="/product/:id" element={<ProductScreen />}></Route>
       <Route path="/cart" element={<CartScreen />}></Route>
+      <Route path="/login" element={<LoginScreen />}></Route>
     </Route>
   )
 );
