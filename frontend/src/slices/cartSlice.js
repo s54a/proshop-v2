346be0021d578743b46bd682db533672ec9a11d7
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-13 03:17:41 +0530

    created dumy placeOrderScreen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -46,6 +46,10 @@ export const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       return updateCart(state);
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return updateCart(state);
+    },
   },
 });
 
@@ -54,6 +58,7 @@ export const {
   removeFromCart,
   saveShippingAddress,
   savePaymentMethod,
+  clearCartItems,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
