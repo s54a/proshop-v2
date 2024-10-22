0ed72a56072034ad793a760399a6830e73324b1b
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-22 05:53:47 +0530

    fixed very big typos

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,4 @@
+import e from "express";
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
 
@@ -106,24 +107,29 @@ const getOrdersById = asyncHandler(async (req, res) => {
  * @access Private
  */
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  const order = await Order.find({ user: req.user._id });
+  try {
+    const order = await Order.findById(req.params.id);
 
-  if (order) {
-    order.isPaid = true;
-    order.paidAt = new Date();
-    order.paymentResult = {
-      id: req.body.id,
-      status: req.body.status,
-      update_time: req.body.update_time,
-      email_address: req.body.email_address,
-    };
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.payer.email_address,
+      };
 
-    const updatedOrder = await order.save();
+      const updatedOrder = await order.save();
 
-    res.status(200).json(updatedOrder);
-  } else {
-    res.status(404);
-    throw new Error("Order not found");
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(401).json(error);
   }
 });
 
