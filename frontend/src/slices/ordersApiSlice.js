5d5e5d993a4de42694f1311c2c59969115ec3020
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-23 12:15:17 +0530

    added table in the orderList

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -10,12 +10,14 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
         body: { ...order },
       }),
     }),
+
     getOrderDetails: builder.query({
       query: (id) => ({
         url: `${ORDERS_URL}/${id}`,
       }),
       keepUnusedDataFor: 5,
     }),
+
     payOrder: builder.mutation({
       query: ({ orderId, details }) => ({
         url: `${ORDERS_URL}/${orderId}/pay`,
@@ -23,17 +25,26 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
         body: { ...details },
       }),
     }),
+
     getPaypalClientId: builder.query({
       query: () => ({
         url: PAYPAL_URL,
       }),
       keepUnusedDataFor: 5,
     }),
+
     getMyOrders: builder.query({
       query: () => ({
         url: `${ORDERS_URL}/mine`,
-        keepUnusedDataFor: 5,
       }),
+      keepUnusedDataFor: 5,
+    }),
+
+    getOrders: builder.query({
+      query: () => ({
+        url: ORDERS_URL,
+      }),
+      keepUnusedDataFor: 5,
     }),
   }),
 });
@@ -44,4 +55,5 @@ export const {
   usePayOrderMutation,
   useGetPaypalClientIdQuery,
   useGetMyOrdersQuery,
+  useGetOrdersQuery,
 } = ordersApiSlice;
