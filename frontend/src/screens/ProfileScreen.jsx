546f5b4846c5c803ec640b1f36660de41c81d42f
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-25 04:05:47 +0530

    typo

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -62,7 +62,7 @@ const ProfileScreen = () => {
           <Form.Group controlId="name" className="my-2">
             <Form.Label>Name</Form.Label>
             <Form.Control
-              type="name"
+              type="text"
               placeholder="Enter Name"
               value={name}
               onChange={(e) => setName(e.target.value)}
