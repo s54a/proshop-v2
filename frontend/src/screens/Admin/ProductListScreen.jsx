6c0b626140a3a2fe9fe1d1c087c7a0005107fe9d
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-24 18:38:59 +0530

    added content in the fiel

--- frontend/src/screens/Admin/ProductListScreen.jsx
+++ frontend/src/screens/Admin/ProductListScreen.jsx
@@ -1,4 +1,96 @@
+import { LinkContainer } from "react-router-bootstrap";
+import { FaTimes, FaEdit, FaTrash } from "react-icons/fa";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import Loader from "../../components/Loader";
+import Message from "../../components/Message";
+import { useGetProductsQuery } from "../../slices/productApiSlice";
+
 const ProductListScreen = () => {
-  return <div>ProductListScreen</div>;
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  const deleteHandler = async (id) => {
+    console.log(id);
+  };
+
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className="btn-sm m-3">
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table
+          striped
+          bordered
+          hover
+          responsive
+          className="table-sm"
+          style={{
+            margin: "0 auto",
+            borderCollapse: "collapse",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr
+                key={product._id}
+                style={{ textAlign: "center", verticalAlign: "middle" }}
+              >
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+
+                <td
+                  style={{
+                    textAlign: "center",
+                    verticalAlign: "middle",
+                  }}
+                >
+                  <LinkContainer to={`/product/${product._id}`}>
+                    <Button className="btn-sm m-2" variant="light">
+                      <FaEdit />
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    className="btn-sm m-2"
+                    variant="danger"
+                    onClick={() => {
+                      deleteHandler(product._id);
+                    }}
+                  >
+                    <FaTrash style={{ color: "white" }} />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
 };
+
 export default ProductListScreen;
