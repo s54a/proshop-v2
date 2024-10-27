df11c91ed71cb4960c0e521530248278f29ca701
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-27 23:58:53 +0530

    search feat backend complete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,11 +10,14 @@ import Product from "../models/productModel.js";
  */
 const getProducts = asyncHandler(async (req, res) => {
   try {
-    const pageSize = 2;
+    const pageSize = 8;
     const page = Number(req.query.pageNumber) || 1;
-    const count = await Product.countDocuments();
+    const keyword = req.query.keyword
+      ? { name: { $regex: req.query.keyword, $options: "i" } }
+      : {};
+    const count = await Product.countDocuments({ ...keyword });
 
-    const products = await Product.find({})
+    const products = await Product.find({ ...keyword })
       .limit(pageSize)
       .skip(pageSize * (page - 1));
 
