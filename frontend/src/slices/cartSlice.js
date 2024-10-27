d54a8f521704471aa4a41219d19d651ed2c49524
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-28 01:59:09 +0530

    stupid ai imported colors denpendency without any reason and broke the code fixed now

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
-import { reset } from "colors";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
