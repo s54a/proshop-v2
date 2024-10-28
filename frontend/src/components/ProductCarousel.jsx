14f642c1204aecb4bd617797ee8c6231988ff44e
Sooraj Gupta <soorajgupta00@gmail.com>
2024-10-28 21:26:43 +0530

    removed the loader which caused 2 loader to appear

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,34 +1,27 @@
 import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
-import Loader from "./Loader";
+// import Loader from "./Loader";
 import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productApiSlice";
 
 const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const { data: products, error } = useGetTopProductsQuery();
 
-  // return isLoading ? (
-  // <Loader />
-  // ) : error ? (
   return error ? (
     <Message variant="danger">{error?.data?.message || error.error}</Message>
-  ) : (
+  ) : products ? (
     <Carousel pause="hover" className="bg-primary mb-4">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
           </Link>
-
           <Carousel.Caption>
-            <h2>
-              {product.name}
-              {product.price}
-            </h2>
+            <h2>{product.name}</h2>
           </Carousel.Caption>
         </Carousel.Item>
       ))}
     </Carousel>
-  );
+  ) : null;
 };
 export default ProductCarousel;
